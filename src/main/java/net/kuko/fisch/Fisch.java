66da1ccff172143475d4892d1f194e0a0f500c5e
kuko010 <kukouwu@proton.me>
2025-12-12 21:25:32 +0100

    MEOW!!!

--- src/main/java/net/kuko/fisch/Fisch.java
+++ src/main/java/net/kuko/fisch/Fisch.java
@@ -2,15 +2,11 @@ package net.kuko.fisch;
 
 import net.kuko.fisch.event.FischEvents;
 import net.kuko.fisch.item.ModItems;
-import net.minecraft.ChatFormatting;
 import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -21,61 +17,37 @@ import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Fisch.MOD_ID)
 public class Fisch {
     public static final String MOD_ID = "fisch";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public static ResourceLocation rl(String s) {
-        return ResourceLocation.fromNamespaceAndPath(MOD_ID, s);
-    }
 
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public Fisch(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::commonSetup);
         NeoForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
 
-        NeoForge.EVENT_BUS.register(new FischEvents());
-        // Register the item to  a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
-        // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
-            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-        }
-
-        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
-
-        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
     }
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-           // event.accept(EXAMPLE_BLOCK_ITEM);
-        }
-        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
-//            event.accept(ModItems.CROOKED_SWORD.get());
-        }
     }
 
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
     }
+
+
+    public static ResourceLocation rl(String s) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, s);
+    }
+
 }
--- src/main/java/net/kuko/fisch/event/FischEvents.java
+++ src/main/java/net/kuko/fisch/event/FischEvents.java
@@ -1,35 +1,21 @@
 package net.kuko.fisch.event;
 
+import net.kuko.fisch.Fisch;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.tick.EntityTickEvent;
 
+@EventBusSubscriber(modid = Fisch.MOD_ID)
 public class FischEvents {
-
-
-    @SubscribeEvent
-    public void onLivingJump(LivingEvent.LivingJumpEvent event) {
-        LivingEntity entity = event.getEntity();
-        if (!entity.level().isClientSide()) {
-            entity.heal(1);
-        }
-    }
-
-    // When i type psf and then tab, it generates    public static final [Cursor Here]
-
-    public void onTick(EntityTickEvent.Pre event) {
+    public static void onTick(EntityTickEvent.Pre event) {
         Entity entity = event.getEntity();
-//        if (entity instanceof )
-
-
     }
 
-    public void onInteract(PlayerInteractEvent.RightClickItem event) {
-        if (!event.getLevel().isClientSide()) {
-
-        }
+    public static void onInteract(PlayerInteractEvent.RightClickItem event) {
     }
 }
